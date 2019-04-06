f516d336a6cccf753b75599b19a26144fdf14b97
sarmahdi <sarmahdi@gmail.com>
2019-04-06 18:03:55 +1000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ReactCRUDApp
-React, Node,js, MongoDB, Express
+React, Node,js, MongoDB ( over the cloud), Express
+
+A simple React App uses mongoDb over MongoDB cloud as Database as a service DAAS, with all CRUD operations.
---
+++ client/src/AppOrig.js
@@ -0,0 +1,52 @@
+// client/src/App.js
+import React, { Component } from "react";
+ import logo from './logo.svg'; 
+ import './App.css';
+
+class App extends Component {
+  render() {
+    return (<div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <div> A client for the backend App : edited in App.js</div>
+            </p>
+            </header>
+          </div>
+          );
+    
+  }
+}
+
+export default App;
+
+
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg'; -->
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
